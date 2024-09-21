065c9316e7ca0afe13125f6e5ca9b09f3da375a9
GabrielOrtega01 <128447535+GabrielOrtega01@users.noreply.github.com>
2024-09-21 03:17:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render('index.html')
+    return render_florWendy('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
